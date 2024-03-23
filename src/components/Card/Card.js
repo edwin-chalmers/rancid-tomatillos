0534b01d126b59ef71f6e464d544331325664568
Edwin Chalmers <edwinchalmers@gmail.com>
2024-03-23 16:29:58 -0600

    Adjusted the API to populate topDesciption state and used Add topDescription state

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -5,7 +5,7 @@ function Card({title, image, id, fetchSelectedMovie}) {
     
 
     return (
-        <figure className='card' onClick={() => fetchSelectedMovie(id)}>
+        <figure className='card' onClick={() => fetchSelectedMovie(id, title)}>
             <img className="poster" id={id} src={image} alt="" height="250px" width="200px"/>
         </figure>
     )
