4ab618298846928c31e037dbd15c57dc3bdfe01f
Asher Spurr <asher.spurr@gmail.com>
2024-03-24 17:46:00 -0600

    feature: add details and CSS to modal

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,13 +1,19 @@
 import './Modal.css'
 
-function Modal({ movie, handleClose }) {
+function Modal({ movie, handleClose}) {
 
     console.log('target Movie', movie)
-    const backdrop = movie.backdrop_path
+    console.log('movie title', movie.title)
 
+    const releaseDate = movie.release_date
+    console.log('release date', releaseDate)
+    // movie.release_date.replaceAll('-', ' ').split(' ').splice(0, 1)
+
+    const backdrop = movie.backdrop_path
     const figBackground = {
         backgroundImage: 'url(' + backdrop + ')',
     }
+
     return (
         <dialog>
             {/* <figure style={figBackground}> */}
@@ -15,9 +21,26 @@ function Modal({ movie, handleClose }) {
                 <img style={figBackground} alt=''></img>
             </div>
             <div className='dialog-contents'>
-                <h2>{movie.title}</h2>
-                <p>{}</p>
-                <button onClick={handleClose}>X</button>
+                <div className='movie-details'>
+                    <h1>{movie.title}</h1>
+                    <p>{movie.overview}</p>
+                    <div className='deep-details'>
+                        {/* change to sematic dl */}
+                        <div>
+                            <p className='details-title'>Rating:</p>
+                            <p>{movie.average_rating}/10</p>
+                        </div>
+                        <div>
+                            <p className='details-title'>Release Date:</p>
+                            <p>{movie.release_date}</p>
+                        </div>
+                        <div>
+                            <p className='details-title'>Genre:</p>
+                            <p>{movie.genres}</p>
+                        </div>
+                    </div>
+                </div>
+                <button onClick={handleClose}>⃪ Back to home</button>
             </div>
             {/* </figure> */}
             <div>
