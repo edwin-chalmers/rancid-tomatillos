3bc66369241f271c0a07932387f47d7dd9f2483a
Asher Spurr <asher.spurr@gmail.com>
2024-03-26 08:40:38 -0600

    Router

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -58,26 +58,10 @@ function App() {
 
   return (
     <BrowserRouter>
-     {!movies.length && (
-        <div className="error-505">
-          <img className="rancid-tomatillo-logo" src={rancidTomatilloLogo} alt="Rancid Tomatillo" />
-          <h1>There was a glitch in the matrix..</h1>
-          <div className='error-message'>
-            <p>{error}. We now know about this issue and are working to fix it.</p>
-            <p>In the meantime, here is what you can do:</p>
-            <p>&nbsp;</p>
-            <ul>
-              <li><b>Refresh the page</b>(Sometimes this helps)</li>
-              <li><b>Try again</b> in 30 minutes</li>
-            </ul>
-          </div>
-        </div>
-      )}
     <header>
       <nav className="Nav-bar">
         <h1>Rancid Tomatillos</h1>
         <NavLink to='/'>Home</NavLink>
-        {/* <NavLink to='/:id'>Modal</NavLink> */}
       </nav>
     </header>
       <main className="App">
