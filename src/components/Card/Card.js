7b9b05f80e55e6355c5373aff328b64df35dc619
Asher Spurr <asher.spurr@gmail.com>
2024-03-27 15:58:40 -0600

    Css styling Home page

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -7,7 +7,7 @@ function Card({ title, image, id }) {
         <>
             <figure className='card' >
                 <NavLink to={`/${id}`}>
-                    <img className="poster" id={id} src={image} alt={`${title} poster`} height="250px" width="200px" />
+                    <img className="poster" id={id} src={image} alt={`${title} poster`} height="350px" width="300px" />
                 </NavLink>
             </figure>
         </>
