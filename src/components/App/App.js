0cefabae8e2b59a2c009f2a165eb29439b13c467
Asher Spurr <asher.spurr@gmail.com>
2024-03-29 17:27:15 -0600

    Move header outside Main tag to follow convention

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -5,15 +5,12 @@ import ErrorPage from '../ErrorPage/ErrorPage'
 import Nav from '../Nav/Nav'
 import { useState, useEffect } from 'react'
 import { fetchData } from '../../apiCalls'
-import { Route, Routes, useNavigate, NavLink } from 'react-router-dom'
+import { Route, Routes, useNavigate } from 'react-router-dom'
 import { formatDate, formatGenre } from '../../utils/utils'
-import rancidTomatilloLogo from '../../images/rancid-tomatillo-logo.png';
-
 
 function App() {
   const [movies, setMovies] = useState([])
   const [topDescription, setTopDescription] = useState({})
-  // const [errorMessage, setErrorMessage] = useState('')
   const navigate = useNavigate()
 
 
@@ -29,7 +26,6 @@ function App() {
           if (topMovie) {
             return fetchData(`movies/${topMovie.id}`)
           } else {
-            // setErrorMessage("Can't fetch the movie :(") 
             navigate("/error", { replace: true })
           }
 
@@ -40,23 +36,14 @@ function App() {
       })
       .catch(error => {
         navigate("/error", { replace: true })
-        // setErrorMessage('404')
         console.log(error.message)
       });
   }, [navigate]);
 
   return (
     <>
-      <header className="header">
-        {/* <nav className="Nav-bar">
-          <img className="rancid-tomatillo-logo" src={rancidTomatilloLogo} alt="Rancid Tomatillo icon" />
-          <h1 className="nav-h1"><NavLink to='/' className="nav-link">Rancid Tomatillos</NavLink></h1>
-        </nav> */}
-      </header>
+      <Nav />
       <main className="App">
-        <header>
-            <Nav />
-          </header>
         <Routes>
           <Route path='/' element={<Home movies={movies} topDescription={topDescription} formatGenre={formatGenre} />} />
           <Route path='/:movieId' element={<Modal formatDate={formatDate} formatGenre={formatGenre} />} />
@@ -64,7 +51,7 @@ function App() {
           <Route path='*' element={<ErrorPage error='*' />} />
         </Routes>
       </main>
-    </>
+      </>
   );
 }
 
