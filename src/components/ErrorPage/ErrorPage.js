cd70114011fca48bd090c1d2bbbbf010d3f5f2d2
Asher Spurr <144856487+AsherSpurr@users.noreply.github.com>
2024-03-30 13:33:23 -0600

    Work in progress

--- src/components/ErrorPage/ErrorPage.js
+++ src/components/ErrorPage/ErrorPage.js
@@ -1,8 +1,20 @@
-
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import "./ErrorPage.css"
 import rancidTomatilloError from '../../images/rancid-tomatillo.png';
 
 function ErrorPage() {
+    // const navigate = useNavigate()
+    // useEffect(() => {
+    //     if (localStorage.getItem('firstLoad' === null)) {
+    //         console.log('first page load')
+    //         localStorage.setItem('firstLoad', 1)
+    //     } else {
+    //         console.log('page refresh')
+    //         localStorage.clear('firstLoad')
+    //         navigate('/', {replace: true})
+    //     }
+    // }, [])
     return (
         <div className="error-505">
             <img className="rancid-tomatillo-error" src={rancidTomatilloError} alt="Rancid Tomatillo broken icon" />
